5ae6c24480dc2ae47f80e52b5b3074c1e4594b24
Gilcimar Barros <gilcimar.barros@hotmail.com>
2022-01-27 22:44:12 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,29 @@
-const meuCabecalho = document.querySelector('h1');
-meuCabecalho.textContent = 'Olá mundo!';
+let minhaImagem = document.querySelector('img');
+
+minhaImagem.onclick = function() {
+    let meuSrc = minhaImagem.getAttribute('src');
+    if(meuSrc === 'imagens/fullstack.png') {
+        minhaImagem.setAttribute ('src','imagens/fullstack2.jpg');
+    } else {
+        minhaImagem.setAttribute ('src','imagens/fullstack.png');
+    }
+}
+
+let meuBotao = document.querySelector('button');
+let meuCabecalho = document.querySelector('h1');
+
+function defineNomeUsuario(){
+    let meuNome = prompt('Por favor, digite seu nome.');
+    localStorage.setitem('nome', meuNome);
+    meuCabecalho.textContent = 'Meu site é legal,' + meuNome;
+}
+
+if(!localStorage.getItem('nome')) {
+    defineNomeUsuario();
+} else {
+    let nomeGuardado = localStorage.getItem('nome');
+    meuCabecalho.textContent = 'Meu site é legal, ' + nomeGuardado;
+}
+
+meuBotao.onclick = function() {defineNomeUsuario;
+}
